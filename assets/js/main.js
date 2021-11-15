9ee0ee32d3e18df364c4805ad1f67ee76542125e
Olga <>
2021-11-15 15:56:54 +0100

    test some brocken youtube links

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", function(){
   const loader = document.querySelector(".loader");
   loader.className += " hidden";
-})
+});
 // Variables
 let checkboxArr = [];
 const language = document.getElementById('language'),
