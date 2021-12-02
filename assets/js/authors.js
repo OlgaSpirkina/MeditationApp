ab52efc5d3a265655e7474330a4019f142d1ab0a
Olga <>
2021-12-02 16:10:17 +0100

    finish authors bio for small and large screen

--- assets/js/authors.js
+++ assets/js/authors.js
@@ -1,4 +1,3 @@
-//import authorsBio from "https://github.com/OlgaSpirkina/MeditationApp/blob/main/authorsBio.json";
 class Carousel{
   /**
   @callback moveCallbacks
@@ -80,9 +79,11 @@ class Carousel{
   }
   next(){
     this.gotoItem(this.currentItem + this.slidesToScroll);
+    document.getElementsByClassName('bio')[0].style.display = "none";
   }
   prev(){
     this.gotoItem(this.currentItem - this.slidesToScroll);
+    document.getElementsByClassName('bio')[0].style.display = "none";
   }
   /**
     moves the carousel to the choosen  element
@@ -143,67 +144,114 @@ class Carousel{
 }
 // End Class Carousel
 //
-document.addEventListener('DOMContentLoaded', function(){
-  let authorsBio;
-  let bioSection = document.getElementsByClassName('bio')[0];
-  let bioParagraphe = document.getElementsByClassName('bio-paragraphe')[0];
-  let closingIcon = document.createElement('i');
-  closingIcon.setAttribute('class', 'far fa-2x fa-window-close icon-close');
-  fetch("https://raw.githubusercontent.com/OlgaSpirkina/MeditationApp/main/authorsBio.json")
-  .then(response => {
-     return response.json();
-  })
-  .then(data => { authorsBio = data });
-  console.log(authorsBio);
-  new Carousel(document.querySelector('#carousel1'),{
-    slidesToScroll: 1,
-    slidesVisible: 3,
-    loop: true
-  });
-  let carouselItem = document.querySelectorAll('figure');
-  for(let i=0; i<carouselItem.length; i++){
-    let divIcon = document.createElement('div');
-    let icon = document.createElement('i');
-    let infoText = document.createElement('p');
-    let textNode = document.createTextNode('hello here');
-    infoText.setAttribute('class', 'hidden-text');
-    divIcon.setAttribute('class', 'div-info');
-    icon.setAttribute('class', 'fas fa-2x fa-info-circle');
-    divIcon.appendChild(icon);
-    divIcon.appendChild(infoText);
-    infoText.appendChild(textNode);
-    carouselItem[i].appendChild(divIcon);
+// Call new Class when data is fetched
+//
+window.onload = async () => {
+  let authorsBio = await fetchAuthorsBio;
+  if(authorsBio){
+    new Carousel(document.querySelector('#carousel1'),{
+      slidesToScroll: 1,
+      slidesVisible: 3,
+      loop: true
+    });
   }
-  let infoIcon = document.querySelectorAll('.fa-info-circle');
-  let divInfoText = document.querySelectorAll('.hidden-text');
-  let cards = document.querySelectorAll('.cards');
-  for(let j=0; j<infoIcon.length; j++){
-    infoIcon[j].addEventListener('click', function(){
-      for(let a=0; a<divInfoText.length; a++){
-        for(let b=0; b<cards.length; b++){
-          if((j === a) && (j === b) && (a === b)){
-            let authorName = cards[b].getAttribute('alt');
-            divInfoText[a].innerHTML = `know more about ${authorName}`;
-            divInfoText[a].classList.add('show-text');
-            setTimeout(function(){ divInfoText[a].classList.remove('show-text'); }, 2000);
-            //document.getElementsByClassName('bio')[0].classList.toggle('show-text');
-            console.log(typeof authorsBio)
-            authorsBio.forEach((author) => {
-              if(author.name === authorName){
-                bioParagraphe.innerHTML = author.bio;
-                bioSection.appendChild(closingIcon);
-                bioSection.style.border = "1px solid #fff";
-                let closingIconElem = document.querySelectorAll('.icon-close')[0];
-                closingIconElem.style.display = "block";
-                closingIconElem.addEventListener('click', function(){
-                  bioSection.style.display = "none";
-                })
-                bioSection.style.display = "block";
-              }
-            })
-          }
+}
+// Get the data: authors' biography
+async function fetchAuthorsBio(){
+  const res = await fetch('https://raw.githubusercontent.com/OlgaSpirkina/MeditationApp/main/authorsBio.json');
+  if (!res.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  const data = await res.json();
+  const parantFigure = document.querySelectorAll('.carousel__item'), // div with figure where p will be appended in small screen
+        bioSection = document.getElementsByClassName('bio')[0], // section to display bio
+        cards = document.querySelectorAll('.cards'); // all the img of authors
+// elements will be presented differently in small or large screen
+  const mql = window.matchMedia('(max-width: 800px)');
+  let mobileWindow = mql.matches;
+// create DOM Elements (p: where the biography will be displayed when large screen) &  (icon)
+  let bioParagrapheElem = document.createElement('p');
+  bioParagrapheElem.setAttribute('class', 'bio-paragraphe');
+  let closingIcon = document.createElement('i');
+  closingIcon.setAttribute('class', 'far fa-2x fa-times-circle icon-close');
+// every .card (img) contains alt attr with author's name needed to append a correspondent text
+  cards.forEach((card, cardIndex) => {
+    if(mobileWindow){
+      let authorNameM = card.getAttribute('alt'); //correct author's name to compare with name came from fetched data
+      parantFigure.forEach((figure, figureIndex) => {
+        console.log(parantFigure.length)
+        if(cardIndex === figureIndex){
+// in small screen create 4 paragraphes
+          let bioP = document.createElement('p');
+          bioP.setAttribute('class', 'bio-p');
+          figure.appendChild(bioP);
+          data.forEach((author) => {
+            if(author.name === authorNameM){
+              let bioParagrapheMobile = document.querySelectorAll('.bio-p');
+              bioParagrapheMobile.forEach((parag, item) => {
+                if(cardIndex === item){
+                  parag.innerHTML = author.bio;
+                }
+              })
+            }
+          })
+        }
+      })
+// when large screen only one paragraphe will be created bioParagrapheElem
+    }else{
+      bioSection.appendChild(bioParagrapheElem);
+      const bioParagraphe = document.getElementsByClassName('bio-paragraphe')[0];
+      for(let i=0; i<parantFigure.length; i++){
+        if(i === cardIndex){
+      // create div containing information icon & hidden text 'know more about...'
+      // append it to the div containing the figure
+          let divIcon = document.createElement('div');
+          let icon = document.createElement('i');
+          let infoText = document.createElement('p');
+          infoText.setAttribute('class', 'hidden-text');
+          divIcon.setAttribute('class', 'div-info');
+          icon.setAttribute('class', 'fas fa-2x fa-info-circle');
+          divIcon.appendChild(icon);
+          divIcon.appendChild(infoText);
+          parantFigure[i].appendChild(divIcon);
         }
       }
-    })
-  }
-})
+    // access newly created icons and divs to display custom text with author's name
+      let infoIcon = document.querySelectorAll('.fa-info-circle');
+      let divInfoText = document.querySelectorAll('.hidden-text');
+      for(let j=0; j<infoIcon.length; j++){
+        infoIcon[j].addEventListener('click', function(){
+          for(let a=0; a<divInfoText.length; a++){
+            if((j === a) && (j === cardIndex) && (a === cardIndex)){
+              let authorName = card.getAttribute('alt');
+              divInfoText[a].innerHTML = `know more about ${authorName}`;
+              divInfoText[a].classList.add('show-text');
+  // make hidden text disappear after 2sec.
+              setTimeout(function(){ divInfoText[a].classList.remove('show-text'); }, 2000);
+              data.forEach((author) => {
+                if(author.name === authorName){
+                  bioParagraphe.innerHTML = author.bio;
+                  bioSection.appendChild(closingIcon);
+                  bioParagraphe.style.padding = "1rem";
+                  bioSection.style.border = "1px solid var(--navbarGrey)";
+                  let closingIconElem = document.querySelectorAll('.icon-close')[0];
+                  closingIconElem.style.display = "block";
+                  closingIconElem.addEventListener('click', function(){
+                    bioSection.style.display = "none";
+                    bioParagraphe.style.padding = "0";
+                  })
+                  bioSection.style.display = "block";
+                }
+              })
+            }
+          }
+        })
+      }
+    }
+  })
+  return data;
+}
+fetchAuthorsBio().catch(error => {
+  error.message;
+});
